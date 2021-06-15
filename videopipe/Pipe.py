bf78948fa715eda3420225825a4d15c4332d0c1b
ryanfjh <ryanfan310@gmail.com>
2021-06-15 16:22:52 -0400

    modified pipeline without testing

--- videopipe/Pipe.py
+++ videopipe/Pipe.py
@@ -6,45 +6,50 @@ import numpy as np
 #
 #First step, read the video
 #
-capture = cv.VideoCapture(sys.argv[1])
-rate = capture.get(cv.CAP_PROP_FPS)#get video frame rate
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-size = (int(capture.get(cv.CAP_PROP_FRAME_WIDTH)),
-        int(capture.get(cv.CAP_PROP_FRAME_HEIGHT)))
-out = cv.VideoWriter('sampleout.avi',fourcc,rate,size,True)
-index =1
-while True:
-    success, frame = capture.read()
-    cv.imshow('Frame', frame)#display current frame-to be removed/modified
+def get_frames():
+    capture = cv.VideoCapture(sys.argv[1])
+    rate = capture.get(cv.CAP_PROP_FPS)#get video frame rate
+
+    capture = cv.VideoCapture(sys.argv[1])
+    frame_list =[]
+    while True:
+        success, frame = capture.read()
+        cv.imshow('Frame', frame)#display current frame-to be removed/modified
+        frame_list.append(frame)
+        if not success:
+            break
+        if cv.waitKey(20) & 0xFF == ord('d'):
+            break
+    
+    
+    capture.release()
+    cv.destroyAllWindows()
+    return frame_list
+
+
+def apply_filter(frame):
     #
     #Apply pre-filter here
     #
+    return frame
+
+def frame_processing(frame_list):
+    processed_frames = []
+    return processed_frames
 
 
-    #
-    #Generate points here
-    #
-
-
+    
+def apply_triangulation(frames):
+    triangulated_frames=[]
     #
     #Do triangulation here
     #
+    return triangulated_frames
 
-
-    #
-    #Apply post-filter here
-    #
-
-    #
-    #Output result here
-    #
-
-    out.write(frame)
-    index += 1
-    if cv.waitKey(20) & 0xFF == ord('d'):
-        break
-    if not success:
-        break
-
-capture.release()
-cv.destroyAllWindows()
+def generate_result(frames,rate,size):
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    #size = (int(capture.get(cv.CAP_PROP_FRAME_WIDTH)),
+    #int(capture.get(cv.CAP_PROP_FRAME_HEIGHT)))
+    out = cv.VideoWriter('sampleout.avi',fourcc,rate,size,True)
+    for frame in frames:
+        out.write(frame)
