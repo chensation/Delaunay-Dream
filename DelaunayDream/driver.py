170905d36d4d9819c8efd27cdbd9754bbb5fc0c2
chensation <fogwhite99@gmail.com>
2021-06-18 01:00:23 -0400

    lmao bro try this

--- DelaunayDream/driver.py
+++ DelaunayDream/driver.py
@@ -1,26 +1,34 @@
 
 # from DelaunayDream.testee.test import plzexport
 from DelaunayDream.triangulation.triangulate import triangulate_frame
+from DelaunayDream.triangulation.get_points import generate_sample_points
 import cv2 as cv
 import numpy as np
 import sys
 
 
 def main():
-    img = cv.imread("DelaunayDream/triangulation/sample.jpg")
+    img = cv.imread("triangulation/m.jpg")
     if img is None:
         sys.exit("Could not read the image.")
 
-    f = open("DelaunayDream/triangulation/coords.txt", "r")
+    # f = open("DelaunayDream/triangulation/coords.txt", "r")
+    # temp_pts = f.read().split(" ")
+    # temp_pts.pop()
+    # temp_pts = [int(i) for i in temp_pts]
+    # f.close()
+    # temp_pts = np.array(temp_pts, dtype=int)
 
-    temp_pts = f.read().split(" ")
-    temp_pts.pop()
-    temp_pts = [int(i) for i in temp_pts]
+    sample_pts = generate_sample_points(img, 2000, 0)
 
-    f.close()
+    test_pts = sample_pts.reshape((-1, 2))
+    for pt in test_pts:
+        cv.circle(img, tuple(pt), 1, (0, 0, 255))
 
-    temp_pts = np.array(temp_pts, dtype=int)
-    trig_img = triangulate_frame(img, temp_pts, scale_factor=0.1)
+    cv.imshow("Display window", img)
+    cv.waitKey(0)
+
+    trig_img = triangulate_frame(img, sample_pts, scale_factor=0.1)
 
     cv.imshow("Display window", trig_img)
     cv.waitKey(0)
