0289fceec74ed4f7ce5a83574c77c251abaee2fd
Devin Mix <63083435+Devin-Mix@user.noreply.github.com>
2021-07-13 01:40:02 -0400

    chore: worked towards optimizing PDS

--- DelaunayDream/triangulation/get_points.py
+++ DelaunayDream/triangulation/get_points.py
@@ -2,12 +2,13 @@ import numpy as np
 from numpy.random import choice
 import cv2 as cv
 from math import sqrt, pow
+from random import randrange
 
 """
 Based on: https://github.com/pmaldonado/PyTri/blob/master/delaunay.py#L56
 """
 
-
+# Deprecated
 def generate_threshold_points(img, max_points, threshold):
     # Threshold (type: float) is the threshold above which points should be sampled for triangulation.
     # The weights of each pixel (as determined by approx_canny) are compared to this value.
@@ -42,13 +43,12 @@ def generate_sample_points(img, max_points, threshold):
 def rowIsIn(row, arr):
     return np.any(np.equal(arr, row).all(1))
 
-def getDisk(src, center, rH, rW):
+def getCanDisk(src, center, rH, rW):
     hOffset = int(np.max(np.array([0, center[0] - (2 * rH)])))
     wOffset = int(np.max(np.array([0, center[1] - (2 * rW)])))
     trimmed = src[hOffset: int(np.min(np.array([src.shape[0], center[0] + (2 * rH)]))),
               wOffset: int(np.min(np.array([src.shape[1], center[1] + (2 * rW)])))]
     candidates = np.argwhere(trimmed > 0) + np.array([hOffset, wOffset])
-    others = np.argwhere(trimmed == 0) + np.array([hOffset, wOffset])
     canResults = candidates[
                  np.logical_and(
                      (((np.power(candidates[:, 0] - center[0], 2) / np.power(rH, 2)) + (
@@ -57,6 +57,14 @@ def getDisk(src, center, rH, rW):
                              np.power(candidates[:, 1] - center[1], 2) / np.power((2 * rW), 2))) <= 1)
                  ), :
                  ]
+    return canResults
+
+def getOthDisk(src, center, rH, rW):
+    hOffset = int(np.max(np.array([0, center[0] - (2 * rH)])))
+    wOffset = int(np.max(np.array([0, center[1] - (2 * rW)])))
+    trimmed = src[hOffset: int(np.min(np.array([src.shape[0], center[0] + (2 * rH)]))),
+              wOffset: int(np.min(np.array([src.shape[1], center[1] + (2 * rW)])))]
+    others = np.argwhere(trimmed == 0) + np.array([hOffset, wOffset])
     othResults = others[
                  np.logical_and(
                      (((np.power(others[:, 0] - center[0], 2) / np.power(rH, 2)) + (
@@ -65,13 +73,14 @@ def getDisk(src, center, rH, rW):
                              np.power(others[:, 1] - center[1], 2) / np.power((2 * rW), 2))) <= 1)
                  ), :
                  ]
-    return canResults, othResults
+    return othResults
 
-def eligible(point, found, rH, rW):
+
+def available(point, found, rH, rW):
     return(
         not(np.any((
-            (np.power(found[:, 0] - point[0, 0], 2) / pow(rH, 2)) +
-            (np.power(found[:, 1] - point[0, 1], 2) / pow(rW, 2))
+            (np.power(found[:, 0] - point[0], 2) / pow(rH, 2)) +
+            (np.power(found[:, 1] - point[1], 2) / pow(rW, 2))
         ) <= 1))
     )
 
@@ -82,70 +91,63 @@ def poisson_sample(n, weights):
     idealRW = round((idealRH * (width / height)))
     candidates = np.argwhere(weights > 0)
     points = np.ndarray(shape=(1, 2))
-    points[0, :] = candidates[choice(candidates.shape[0], 1), :][0]
+    points[0, :] = candidates[randrange(0, candidates.shape[0]), :]
 
     # Now have initial point to start expanding from, list of candidates to choose from, and weights.
 
     numPoints = 1
     eligiblePoints = np.ndarray(shape = (1, 2))
     eligiblePoints[0, :] = points
-
-    ##print(np.argwhere(weights > 0))
-    #input()
+    unavailable = np.zeros_like(weights, dtype = bool)
+    unavailable[points[0, :].astype(int)] = True
 
     while numPoints < n and eligiblePoints.shape[0] > 0:
-        sourcePointRow = choice(eligiblePoints.shape[0], 1)
-        sourcePoint = eligiblePoints[sourcePointRow, :][0]
-        primeTargets, otherTargets = getDisk(weights, sourcePoint, idealRH, idealRW)
+        sourcePointRow = randrange(0, eligiblePoints.shape[0])
+        sourcePoint = eligiblePoints[sourcePointRow, :]
+        primeTargets= getCanDisk(weights, sourcePoint, idealRH, idealRW)
         attempts = 0
-        #print("Prime: ")
-        #print(primeTargets.shape[0])
         while primeTargets.shape[0] > 0:
             if attempts == 15:
                 primeTargets = np.empty([0, 0])
                 continue
-            targetRow = choice(primeTargets.shape[0], 1)
-            #print("Picked target row")
+            targetRow = randrange(0, primeTargets.shape[0])
             target = primeTargets[targetRow, :]
-            if eligible(target, points, idealRH, idealRW):
+            isUnavailable = unavailable[target[0], target[1]]
+            if not(isUnavailable) and available(target, points, idealRH, idealRW):
                 points = np.vstack((points, target))
+                unavailable[target[0], target[1]] = True
                 eligiblePoints = np.vstack((eligiblePoints, target))
                 numPoints = numPoints + 1
-                #print("Point found")
-                #print(numPoints)
                 break
             else:
+                if not(isUnavailable):
+                    unavailable[target[0], target[1]] = True
                 primeTargets = np.delete(primeTargets, targetRow, 0)
                 attempts = attempts + 1
-            #print("Determined eligibility")
         else:
+            otherTargets = getOthDisk(weights, sourcePoint, idealRH, idealRW)
             attempts = 0
-            #print("Other: ")
-            #print(otherTargets.shape[0])
             while otherTargets.shape[0] > 0:
                 if attempts == 15:
-                    #print("Point failed, clearing targets.")
                     otherTargets = np.empty([0, 0])
                     continue
-                targetRow = choice(otherTargets.shape[0], 1)
+                targetRow = randrange(0, otherTargets.shape[0])
                 target = otherTargets[targetRow, :]
-                #print("Found other-type target")
-                if eligible(target, points, idealRH, idealRW):
+                isUnavailable = unavailable[target[0], target[1]]
+                if not(isUnavailable) and available(target, points, idealRH, idealRW):
                     points = np.vstack((points, target))
+                    unavailable[target[0], target[1]] = True
                     eligiblePoints = np.vstack((eligiblePoints, target))
                     numPoints = numPoints + 1
-                    #print("Point found")
-                    #print(numPoints)
                     break
                 else:
+                    if not(isUnavailable):
+                        unavailable[target[0], target[1]] = True
                     otherTargets = np.delete(otherTargets, targetRow, 0)
                     attempts = attempts + 1
             else:
                 eligiblePoints = np.delete(eligiblePoints, sourcePointRow, 0)
-                #print("Row deleted.")
-                #print(eligiblePoints)
 
-    #print("Returning")
     return np.fliplr(points.astype(int))
 
 def threshold_sample(n, weights):
