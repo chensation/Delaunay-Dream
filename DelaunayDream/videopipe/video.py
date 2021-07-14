28f8b7a44b75b34c3bce60121dc4a011956b9d68
chensation <fogwhite99@gmail.com>
2021-07-14 18:21:46 -0400

    feat: reduced multiprocessing overhead

--- DelaunayDream/videopipe/video.py
+++ DelaunayDream/videopipe/video.py
@@ -21,9 +21,10 @@ class Video:
         """ read the video according to filename,
             return list containing all frames
         """
-        # del self.frame_list
-        # del  self.result_frames
-        placeHolder = []
+        self.frame_list = ()
+        self.result_frames = ()
+
+        temp_array = []
         cap = cv.VideoCapture(self.filename)
         self.fps = math.ceil(cap.get(cv.CAP_PROP_FPS)) # get video frame rate, use ceil as 23.976 fps is a popular format
         self.fourcc = cv.VideoWriter_fourcc(*'XVID')
@@ -36,7 +37,7 @@ class Video:
             if not success:
                 break
             # cv.imshow('Frame', frame)#display current frame-to be removed/modified
-            placeHolder.append(frame)
+            temp_array.append(frame)
             # self.frame_list.append(frame)
             # self.result_frames.append(frame, axis=0)
 
@@ -44,9 +45,8 @@ class Video:
             #     break
         cap.release()
 
-        self.frame_list = np.array(placeHolder)
-        self.result_frames = np.array(placeHolder)
-        del placeHolder
+        self.frame_list = np.asarray(temp_array)
+        self.result_frames = np.array(temp_array)
         # cv.destroyAllWindows()
 
     def export_video(self, filename, have_color=True):
@@ -56,7 +56,8 @@ class Video:
 
     # TODO: move this into load_video once the gui is ready
     def apply_output_framerate(self, alt_fps):
-        placeHolder = []
+        temp_array = []
+        self.result_frames = ()
         self.output_fps = alt_fps
         step_size = int(self.fps/self.output_fps)
 
@@ -66,9 +67,9 @@ class Video:
 
         for index, frame in enumerate(self.frame_list):
             if index % step_size == 0:
-                placeHolder.append(frame)
+                temp_array.append(frame)
 
-        self.result_frames = np.array(placeHolder)
+        self.result_frames = np.asarray(temp_array)
 
 
     # def process_video(self, func):
@@ -81,19 +82,20 @@ class Video:
 
     def process_video(self, func):
         # inputFrames = self.result_frames
-        Q1 = len(self.result_frames)//4
+        Q1 = self.result_frames.shape[0]//4
         Q2 = Q1 * 2
-        Q3 = Q1 *3
+        Q3 = Q1 * 3
 
         # inputArray = np.array(inputFrames)
         shm = shared_memory.SharedMemory(create=True, size=self.result_frames.nbytes)
-        sharedArray = np.ndarray(self.result_frames.shape, dtype=self.result_frames.dtype, buffer=shm.buf)
-        sharedArray = self.result_frames.copy()
+        shared_array = np.ndarray(self.result_frames.shape, dtype=self.result_frames.dtype, buffer=shm.buf)
+        shared_array[:] = self.result_frames[:]
+        self.result_frames = ()
 
-        Q1out = multiprocessing.Process(target = self.process_frames_sharedMem, args=(func, shm.name, inputFrames[:Q1], 1, Q1, sharedArray.shape, sharedArray.dtype))
-        Q2out = multiprocessing.Process(target = self.process_frames_sharedMem, args=(func, shm.name, inputFrames[Q1:Q2], 2, Q1, sharedArray.shape, sharedArray.dtype))
-        Q3out = multiprocessing.Process(target = self.process_frames_sharedMem, args=(func, shm.name, inputFrames[Q2:Q3], 3, Q1, sharedArray.shape, sharedArray.dtype))
-        Q4out = multiprocessing.Process(target = self.process_frames_sharedMem, args=(func, shm.name, inputFrames[Q3:], 4, Q1, sharedArray.shape, sharedArray.dtype))
+        Q1out = multiprocessing.Process(target=process_shared_mem, args=(func, shm.name, 0, Q1, shared_array.shape, shared_array.dtype))
+        Q2out = multiprocessing.Process(target=process_shared_mem, args=(func, shm.name, Q1, Q2, shared_array.shape, shared_array.dtype))
+        Q3out = multiprocessing.Process(target=process_shared_mem, args=(func, shm.name, Q2, Q3, shared_array.shape, shared_array.dtype))
+        Q4out = multiprocessing.Process(target=process_shared_mem, args=(func, shm.name, Q3, shared_array.shape[0], shared_array.shape, shared_array.dtype))
 
         Q1out.start()
         Q2out.start()
@@ -105,61 +107,64 @@ class Video:
         Q3out.join()
         Q4out.join()
 
-        self.result_frames = sharedArray.copy()
+        self.result_frames = shared_array.copy()
+        del shared_array
 
-        del sharedArray
         shm.close()
         shm.unlink()
+
+
+def process_shared_mem(func, shared_mem_name, begin, end, shape, data_type):
+    # print("Starting process", begin )
+    shm = shared_memory.SharedMemory(name=shared_mem_name)
+    shared_array = np.ndarray(shape, dtype=data_type, buffer=shm.buf)
+
+    for index in range(begin, end):
+        shared_array[index] = func(shared_array[index])
+    # inputList = list(inputArray)
+    # processedList = list(map(func, inputList))
+    # processedList = self.process_frames(inputList, func)
+
+    # processedArray = np.array(processedList)
+    # if q==1:
+    #     sharedArray[:q*Q1] = processedArray[:]
+    # elif q==4:
+    #     sharedArray[(q-1)*Q1:] = processedArray[:]
+    # else:
+    #     sharedArray[(q-1)*Q1:q*Q1] = processedArray[:]
+
+    shm.close()
+    # print("Q", begin, "processed")
+
+    # def process_frames(self, inputFrames, func):
+    #     outputFrames = [None]*len(inputFrames)
+    #     # Q1 = len(outputFrames)//4
+    #     # print("Processing frames")
+    #     for index, frame in enumerate(inputFrames):
+    #         # if index % Q1 == 0: print(" . ", end='', flush=True)
+    #         outputFrames[index] = func(frame)
+    #     # print("Num Frames processed: ", len(outputFrames))
+    #     return outputFrames
     
-    def process_frames_sharedMem(self, func, sharedMemName, inputArray, q, Q1, shape, dataType):
-        print("Starting process", q )
-        shm = shared_memory.SharedMemory(name=sharedMemName)
-        sharedArray = np.ndarray(shape, dtype=dataType, buffer=shm.buf)
-
-        inputList = list(inputArray)
-        # processedList = list(map(func, inputList))
-        processedList = self.process_frames(inputList, func)
-
-        processedArray = np.array(processedList)
-        if q==1:
-            sharedArray[:q*Q1] = processedArray[:]
-        elif q==4:
-            sharedArray[(q-1)*Q1:] = processedArray[:]
-        else:
-            sharedArray[(q-1)*Q1:q*Q1] = processedArray[:]
-
-        shm.close()
-        print("Q", q, "processed")
-
-    def process_frames(self, inputFrames, func):
-        outputFrames = [None]*len(inputFrames)
-        # Q1 = len(outputFrames)//4
-        # print("Processing frames")
-        for index, frame in enumerate(inputFrames):
-            # if index % Q1 == 0: print(" . ", end='', flush=True)
-            outputFrames[index] = func(frame)
-        # print("Num Frames processed: ", len(outputFrames))
-        return outputFrames
-    
-
-    """ try: output video with specified fps
-    """
-    def generate_with_fps(self, filename,alt_fps):
-        out_color = cv.VideoWriter(filename, self.fourcc, alt_fps, self.video_size, True)
-        step_size = self.fps/alt_fps
-        frame_buf = None
-        out_frames = []
-        step = 1
-        for frame in self.result_frames:
-            if step == 1:
-                frame_buf = frame
-                #out_color.write(frame)
-                out_frames.append(frame)
-            elif step == step_size:
-                step = 0
-                #out_color.write(frame_buf)
-            # else:
-            #   out_color.write(frame_buf)
-            step += 1
-        for frame in out_frames:
-            out_color.write(frame)
+    #
+    # """ try: output video with specified fps
+    # """
+    # def generate_with_fps(self, filename,alt_fps):
+    #     out_color = cv.VideoWriter(filename, self.fourcc, alt_fps, self.video_size, True)
+    #     step_size = self.fps/alt_fps
+    #     frame_buf = None
+    #     out_frames = []
+    #     step = 1
+    #     for frame in self.result_frames:
+    #         if step == 1:
+    #             frame_buf = frame
+    #             #out_color.write(frame)
+    #             out_frames.append(frame)
+    #         elif step == step_size:
+    #             step = 0
+    #             #out_color.write(frame_buf)
+    #         # else:
+    #         #   out_color.write(frame_buf)
+    #         step += 1
+    #     for frame in out_frames:
+    #         out_color.write(frame)
