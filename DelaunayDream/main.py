b38dd73a167a6e91160ac78732faccf56c6d6ba1
Octavio Ochoa <octavio.ochoa@ufl.edu>
2021-07-15 13:39:00 -0700

    GUI Spacing Fixed, Video Window expands to MainWindow size, Added popup dialog, interchangeable play/pause, added stylesheet

--- DelaunayDream/main.py
+++ DelaunayDream/main.py
@@ -4,6 +4,8 @@ import os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from DelaunayDream.gui.gui import Ui_MainWindow
+from DelaunayDream.gui.dialog import Ui_Dialog ####Octavio's Changes####
+from DelaunayDream.gui.stylesheet import StyleSheet  ####Octavio's Changes####
 from DelaunayDream.triangulation.triangulation import Triangulation
 from DelaunayDream.videopipe.video import Video
 from DelaunayDream.videopipe.process import Process
@@ -86,9 +88,31 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         self.original = None
         self.video = Video()
 
+        ####Octavio's Changes####
+        self.play = True
+        self.temp_filename = ""
+        self.width = self.height = 0
+        self.mode = False
+
+        # Pop-up Dialog       
+        self.dialog = QtWidgets.QDialog(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self.dialog)
+        self.ui.browse_button.clicked.connect(self.set_filename)
+        self.ui.file_lineEdit.textChanged[str].connect(self.onChanged)
+        self.ui.frame_rate_selector.highlighted['QString'].connect(self.set_frame_rate)
+        self.ui.confirm_cancel_button.accepted.connect(self.thread_load_video)
+        self.ui.confirm_cancel_button.accepted.connect(self.dialog.close)
+        self.ui.confirm_cancel_button.rejected.connect(self.dialog.close)
+        
+        #Video Playback
+        self.play_button.clicked.connect(self.set_play_button)
+        self.stop_button.clicked.connect(self.dark_light_mode)
+        #########################
+
         # TODO: remove these two once the gui is ready
-        self.frame_rate_spinBox.valueChanged['int'].connect(self.set_frame_rate)
-        self.frame_rate_spinBox.setValue(self.video.fps)
+        # self.frame_rate_spinBox.valueChanged['int'].connect(self.set_frame_rate)
+        # self.frame_rate_spinBox.setValue(self.video.fps)
 
         self.hue_spinBox.valueChanged['int'].connect(self.set_hue)
         self.saturation_spinBox.valueChanged['int'].connect(self.set_saturation)
@@ -102,7 +126,7 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         self.thickness_spinBox.valueChanged['int'].connect(self.set_line_thickness)
 
         self.apply_button.clicked.connect(self.thread_process_video)
-        self.open_button.clicked.connect(self.thread_load_video)
+        self.open_button.clicked.connect(self.open_dialog) ####Octavio's Changes####
         self.export_button.setEnabled(False)
         self.export_button.clicked.connect(self.thread_export_video)
 
@@ -120,7 +144,11 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
     @_update_func # TODO: remove this after gui is ready
     def set_frame_rate(self, frame_rate):
-        self.video.output_fps = frame_rate
+        if frame_rate == "Default": ####Octavio's Changes####        
+            self.video.output_fps = 0 ####Octavio's Changes####
+        else: ####Octavio's Changes####
+            self.video.output_fps = int(frame_rate) ####Octavio's Changes####
+        print(self.video.output_fps) ####Octavio's Changes####
 
     @_update_func
     def set_hue(self, hue):
@@ -166,8 +194,41 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         to_qt = QtGui.QImage(image, image.shape[1], image.shape[0], image.strides[0], QtGui.QImage.Format_RGB888)
-        pic = to_qt.scaled(700, 700, QtCore.Qt.KeepAspectRatio)
+        pic = to_qt.scaled(self.width, self.height, QtCore.Qt.KeepAspectRatio) ####Octavio's Changes####
         self.video_player.setPixmap(QtGui.QPixmap.fromImage(pic))
+
+    ####Octavio's Changes####
+    def resizeEvent(self, event):
+        self.width = self.video_player.width()
+        self.height = self.video_player.height()
+
+    def open_dialog(self):
+        # self.setStyleSheet(StyleSheet().stylesheet2)
+        self.dialog.exec_()
+
+    def set_filename(self):
+        self.temp_filename = QtWidgets.QFileDialog.getOpenFileName(filter="Video files(*.*)")[0]
+        self.ui.file_lineEdit.setText(self.temp_filename)
+
+    def onChanged(self, text):
+        self.temp_filename = text
+
+    def set_play_button(self):
+        if self.play == True:
+            self.play = False
+            self.play_button.setText("Pause")
+        else:
+            self.play = True
+            self.play_button.setText("Play")
+    
+    def dark_light_mode(self):
+        if self.mode == True: 
+            self.setStyleSheet(StyleSheet().light_mode)
+            self.mode = False
+        else:
+            self.setStyleSheet(StyleSheet().dark_mode)
+            self.mode = True
+    #########################
 
     def on_receving_msg(self, s):
         self.update_console_message(s)
@@ -195,7 +256,7 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         self.video = v
         self.have_file = True
         self.frame =self.video.frame_list[0]
-        self.frame_rate_spinBox.setValue(self.video.fps)
+        # self.frame_rate_spinBox.setValue(self.video.fps)
         self.update()
         self.export_button.setEnabled(True)
         self.open_button.setEnabled(True)
@@ -214,8 +275,10 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
     # TODO: don't create a new object every time these functions are called
 
     def thread_load_video(self):
-        self.update_console_message("Choose a file to open")
-        filename = QtWidgets.QFileDialog.getOpenFileName(filter="Video files(*.*)")[0]
+        # self.update_console_message("Choose a file to open")
+        # filename = QtWidgets.QFileDialog.getOpenFileName(filter="Video files(*.*)")[0]
+        self.open_button.setEnabled(False) ####Octavio's Changes####
+        filename = self.temp_filename ####Octavio's Changes####
         self.worker = load_worker(filename)
         self.worker.load_in_process.connect(self.on_loading)
         self.worker.load_finished.connect(self.on_load_finished)
@@ -243,6 +306,7 @@ class GuiWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(StyleSheet().light_mode)
     gui = GuiWindow()
     gui.show()
     sys.exit(app.exec_())
