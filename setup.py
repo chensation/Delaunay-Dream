05d3a5589473d64c915ec638eaa587d30a19eadf
manthi4 <sumanth.phi2000@gmail.com>
2021-08-01 14:12:09 -0400

    updated version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extensions = [
 
 setup(
     name="DelaunayDream",
-    version="1.0.1",
+    version="1.0.2",
     packages=["DelaunayDream", "DelaunayDream.gui", "DelaunayDream.triangulation", "DelaunayDream.videopipe"],
     url="https://github.com/chensation/Delaunay-Dream.git",
     author="drowning_princesses",
